280e8148943211661728c601b43b8c3c5cc425e7
Alex Martinez <alexmartinez0071234@gmail.com>
2022-07-08 16:04:05 -0400

    Added in error handling to greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("No name was given")
+	}
+
+	// If a name is received, return a value that embeds the name in a greeting message
 	var message string = fmt.Sprintf("Hi, %v. Welcome!", name)
 	
-	return message
+	return message, nil
 }
